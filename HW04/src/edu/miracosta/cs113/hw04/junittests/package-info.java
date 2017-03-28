c1a162949fd4fad03034502c14f213637d724dde
elangeloscuro1 <elangeloscuro1@gmail.com>
2017-03-28 16:11:29 -0700

    delete 1

--- HW04/src/edu/miracosta/cs113/hw04/junittests/package-info.java
+++
@@ -1,9 +0,0 @@
-/**
- * This package is a JunitTest package that contains the class 
- * MyDoubleLinkedListJUnitTest which is used to test each method 
- * of the class MyDoubleLinkedList using the class Student.
- */
-/**
- * @author Angel Tapia
- */
-package edu.miracosta.cs113.hw04.junittests ;
