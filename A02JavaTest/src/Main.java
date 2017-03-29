91e2daa4ef79635430f41aa9271fa4ce26de4af1
ADTV <ADTV@DESKTOP-07KVJF5>
2017-03-28 20:16:40 -0700

    0004

--- A02JavaTest/src/Main.java
+++ A02JavaTest/src/Main.java
@@ -9,12 +9,8 @@ public class Main
 		Scanner input = new Scanner(System.in);
 
 		System.out.print("Enter a number: ");
-		double number1 = input.nextDouble();
-
-		System.out.print("Enter second number: ");
-		double number2 = input.nextDouble();
-
-		double product = number1 * number2;
-		System.out.printf("The product of both numbers is: %f", product);
+		int number = input.nextInt() ;
+		
+		System.out.println("You entered: " + number) ;
 	}
 }
