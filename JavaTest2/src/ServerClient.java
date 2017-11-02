df92436746df7c4da60d1cd12baaffd9e0c9cfe7
ADTV <ADTV@DESKTOP-07KVJF5>
2017-11-02 06:42:28 -0700

    11/2/1017 ==>3

--- JavaTest2/src/ServerClient.java
+++ JavaTest2/src/ServerClient.java
@@ -1,6 +1,5 @@
 import java.io.BufferedReader ;
 import java.io.DataOutputStream ;
-import java.io.IOException ;
 import java.io.InputStreamReader ;
 import java.net.ServerSocket ;
 import java.net.Socket ;
@@ -8,52 +7,50 @@ import java.net.Socket ;
 @SuppressWarnings("resource")
 public class ServerClient
 {
-	private static final String HOSTNAME = "localhost" ;
-	private static final int PORT = 7654 ;	
-	
-	public static void main(String[] args)
+	private static final String HOSTNAME = "ip6localhost" ;// 127.0.0.1/localhost
+	private static final int PORT = 7654 ;
+
+	public static void main(String args[])
 	{
-		//server() ;
-		client() ;
+		// server() ;
+		// client() ;
 	}// END of main
-	
-	
+
 	public static void server()
 	{
 		boolean flag = true ;
 		while (flag)
-		try
-		{
-			System.out.println("------------------------------------") ;
-			System.out.println("Waiting for a connection on port " + PORT + ".") ;
+			try
+			{
+				System.out.println("------------------------------------") ;
+				System.out.println("Waiting for a connection on port " + PORT + ".") ;
 
-			ServerSocket serverSock = new ServerSocket(PORT) ;
-			Socket connectionSock = serverSock.accept() ;
-			BufferedReader clientInput = new BufferedReader(new InputStreamReader(connectionSock.getInputStream())) ;
-			DataOutputStream clientOutput = new DataOutputStream(connectionSock.getOutputStream()) ;
+				ServerSocket serverSock = new ServerSocket(PORT) ;
+				Socket connectionSock = serverSock.accept() ;
+				BufferedReader clientInput = new BufferedReader(
+						new InputStreamReader(connectionSock.getInputStream())) ;
+				DataOutputStream clientOutput = new DataOutputStream(connectionSock.getOutputStream()) ;
 
-			System.out.println("Connection made, waiting data from client: ") ;
-			System.out.println("CLIENT: " + clientInput.readLine()) ;
-			String input = new java.util.Scanner(System.in).nextLine() + "\n" ;
-			
-			flag = input.charAt(0) != 'x' ;
-			clientOutput.writeBytes(input) ;
+				System.out.println("Connection made, waiting data from client: ") ;
+				System.out.println("CLIENT: " + clientInput.readLine()) ;
+				String input = new java.util.Scanner(System.in).nextLine() + "\n" ;
 
-			clientOutput.close() ; 
-			clientInput.close() ;
-			connectionSock.close() ;
-			serverSock.close() ;
-		}
-		catch (IOException e)
-		{
-			System.out.println("ERROR: DateServer ==> :" + e.getMessage()) ;
-			System.exit(0) ;
-		}
+				flag = input.charAt(0) != 'x' ;
+				clientOutput.writeBytes(input) ;
 
-	}// END of server	
-	
-	
-	
+				clientOutput.close() ;
+				clientInput.close() ;
+				connectionSock.close() ;
+				serverSock.close() ;
+			}
+			catch (Exception e)
+			{
+				System.out.println("ERROR: DateServer ==> :" + e.getMessage()) ;
+				System.exit(0) ;
+			}
+
+	}// END of server
+
 	public static void client()
 	{
 		boolean flag = true ;
@@ -65,14 +62,16 @@ public class ServerClient
 				System.out.println("Connecting to server on port " + PORT) ;
 
 				Socket connectionSock = new Socket(HOSTNAME, PORT) ;
-				BufferedReader serverInput = new BufferedReader(new InputStreamReader(connectionSock.getInputStream())) ;
+
+				BufferedReader serverInput = new BufferedReader(
+						new InputStreamReader(connectionSock.getInputStream())) ;
 				DataOutputStream serverOutput = new DataOutputStream(connectionSock.getOutputStream()) ;
 
 				System.out.print("Connection made, Send data to server: ") ;
 				String input = new java.util.Scanner(System.in).nextLine() + "\n" ;
 				flag = input.charAt(0) != 'x' ;
 				serverOutput.writeBytes(input) ;
-				
+
 				String serverData = serverInput.readLine() ;
 				System.out.println("SERVER: " + serverData) ;
 
@@ -80,31 +79,12 @@ public class ServerClient
 				serverInput.close() ;
 				connectionSock.close() ;
 			}
-			catch (IOException e)
+			catch (Exception e)
 			{
 				System.out.println("ERROR: DateClient ==> : " + e.getMessage()) ;
 				System.exit(0) ;
 			}
 		}
 	}// END of client
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
 
 }
